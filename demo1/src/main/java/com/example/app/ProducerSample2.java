e4ee0ee9c1736cd61503355d9fc6e48e6e917850
hhgzxn <hhgzxn@gmail.com>
2018-12-09 23:00:06 +0800

    添加demo1

---
+++ demo1/src/main/java/com/example/app/ProducerSample2.java
@@ -0,0 +1,42 @@
+package com.example.app;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+* @ClassName: ProducerSample2
+* @description: 
+* @author: Tony.Cheung
+* @create: 2018/12/9 22:56
+**/
+public class ProducerSample2 {
+
+    /**
+     * @method: main
+     * @description: ${description}
+     * @date: 2018/12/9 22:52
+     * @author: ${author}
+     */
+    public static void main(String[] args) {
+        Map<String, Object> props = new HashMap<String, Object>();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("zk.connect", "127.0.0.1:2181");
+
+        String topic = "test-topic";
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        producer.send(new ProducerRecord<String, String>(topic, "idea-key2", "java-message 1"));
+        producer.send(new ProducerRecord<String, String>(topic, "idea-key2", "java-message 2"));
+        producer.send(new ProducerRecord<String, String>(topic, "idea-key2", "java-message 3"));
+        producer.close();
+    }
+}
